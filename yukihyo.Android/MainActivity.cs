27bac7fa707579dd33f68a1ba0712eed3ac19e65
dylandasilva1999 <55394640+dylandasilva1999@users.noreply.github.com>
2021-03-06 17:01:34 +0200

    Fix LottieAnimations Dependency
    -Reverted to an older version.

--- yukihyo.Android/MainActivity.cs
+++ yukihyo.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Lottie.Forms.Droid;
 using Android.Util;
 
 namespace yukihyo.Droid
@@ -22,6 +23,9 @@ namespace yukihyo.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            AnimationViewRenderer.Init();
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
