ba00fc02a89541f71ab94958987bc1992e5fb5c6
dylandasilva1999 <55394640+dylandasilva1999@users.noreply.github.com>
2021-03-24 09:29:20 +0200

    Add Habitat Unit Test

---
+++ yukihyoTests/Objects/HabitatStatesTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using yukihyo.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace yukihyo.Objects.Tests
+{
+    [TestFixture()]
+    public class HabitatStatesTests
+    {
+        [Test()]
+        public void GetHabitatStringTest()
+        {
+            //ARRANGE
+            string yukihyoString = "neutral";
+            var expectedState = YukihyoState.neutral;
+
+            //ACT
+            var result = YukihyoStates.GetYukihyoState(yukihyoString);
+
+            //ASSERT
+            Assert.AreEqual(expectedState, result);
+        }
+
+        [Test()]
+        public void GetHabitatStateTest()
+        {
+            //ARRANGE
+            string yukihyoString = "neutral";
+            var expectedState = YukihyoState.neutral;
+
+            //ACT
+            var result = YukihyoStates.GetYukihyoState(yukihyoString);
+
+            //ASSERT
+            Assert.AreEqual(expectedState, result);
+        }
+    }
+}
